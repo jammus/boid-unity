fb7ba62a0998e6d8dd8a7119c750b715b1deb882
James Scott <jamess@last.fm>
2017-05-01 18:55:45 +0100

    Single boid moving a central point
    Restrict the z axis acceleration to make movement a bit more interesting

---
+++ Assets/Models/Boid.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+
+	private Vector3 velocity;
+	private Vector3 maxVelocity = new Vector3(0.5f, 0.5f, 0.5f);
+	private Vector3 maxAcceleration = new Vector3(0.005f, 0.005f, 0.002f);
+	private Vector3 initialPosition;
+	private float maxDistance = 3.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		velocity = new Vector3(0.2f, 0.2f, 0.2f);
+		initialPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		var distanceFromOrigin = Vector3.Distance(transform.position, initialPosition);
+		if (distanceFromOrigin > maxDistance)
+		{
+			moveTo(initialPosition);
+		}
+		transform.position += velocity;
+	}
+
+	private void moveTo(Vector3 target)
+	{
+		var acceleration = new Vector3(target.x, target.y, target.z);
+		acceleration -= transform.position;
+		acceleration = max(acceleration.normalized, maxAcceleration);
+		velocity += acceleration;
+		velocity = max(velocity, maxVelocity);
+	}
+
+	private Vector3 max(Vector3 v1, Vector3 v2) 
+	{
+		var maxVector = new Vector3(v1.x, v1.y, v1.z);
+		maxVector.x = Mathf.Max(maxVector.x, -v2.x);
+		maxVector.y = Mathf.Max(maxVector.y, -v2.y);
+		maxVector.z = Mathf.Max(maxVector.z, -v2.z);
+		maxVector.x = Mathf.Min(maxVector.x, v2.x);
+		maxVector.y = Mathf.Min(maxVector.y, v2.y);
+		maxVector.z = Mathf.Min(maxVector.z, v2.z);
+		return maxVector;
+	}
+}
